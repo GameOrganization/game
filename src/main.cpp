0730ab4c861072200d4ef38678722b73f88ede7d
420yoloscope <epsillon42@yahoo.com>
2014-06-11 13:15:29 -0400

    Added Vec2f
    Tested and appears to work

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "Vec3f.h"
+#include "Vec2f.h"
 
 //returns float between -2.0f, and 2.0f, multiples of .25
 float randFloat() {
@@ -24,4 +25,18 @@ int main(int argc, char *argv[]) {
     std::cout<< "a dot b: " <<Vec3f::dot(a,b)<<std::endl;
     std::cout<< "a cross b: " <<Vec3f::cross(a,b)<<std::endl;
     std::cout << "Angle between " <<a<<" and "<<b<<": "<<Vec3f::angle(a, b) << std::endl;
+
+    std::cout << "================================="<<std::endl;
+
+    Vec2f c(randFloat(), randFloat());
+    Vec2f d(randFloat(), randFloat());
+    std::cout << "c: " << c << std::endl;
+    std::cout << "d: " << d << std::endl;
+    std::cout << "Magnitude of c: " << c.mag() << std::endl;
+    std::cout << "Magnitude of d: " << d.mag() << std::endl;
+    std::cout << "Normalized c: " << c.norm() << std::endl;
+    std::cout << "Normalized d: " << d.norm() << std::endl;
+    std::cout << "c + d: " << c+d << std::endl;
+    std::cout<< "c dot d: " <<Vec2f::dot(c,d)<<std::endl;
+    std::cout << "Angle between " <<c<<" and "<<d<<": "<<Vec2f::angle(c, d) << std::endl;
 }
